a324a54b92b97c88d77b8e631aed36a75fab6d77
Mutaz9 <mutazbadr333@gmail.com>
2022-07-10 18:48:43 -0400

    fix: redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 cd project-m
 git fetch && git reset origin/main --hard
-python3.10 -m venv venv
-source venv/bin/activate
-python3.10 -m pip install --upgrade pip
-python3.10 -m pip install -r requirements.txt
-systemctl daemon-reload
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
